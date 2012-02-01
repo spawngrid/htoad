f5fbbc1e5a78b4d84dd87afe7b0211cdcf8ec18f
Yurii Rashkovskii <yrashk@gmail.com>
2012-01-31 21:09:29 -0800

    Adds {print, Text} rule

---
+++ apps/htoad/src/htoad_io.erl
@@ -0,0 +1,14 @@
+-module(htoad_io).
+-include_lib("htoad/include/htoad.hrl").
+-include_lib("htoad/include/stdlib.hrl").
+
+-export([init/2, print/2]).
+-rules([init, print]).
+
+init(Engine, #init{}) ->
+    lager:debug("Initialized htoad_io"),
+    Engine.
+
+print(Engine, {print, Text}) ->
+    io:format(Text),
+    Engine.
