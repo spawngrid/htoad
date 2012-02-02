096b5a2e57ac49a218b1d4fea2ed1e7397a3f85d
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-02-01 16:00:16 -0800

    Adds file/1 and render/2,3,4 helpers

--- apps/htoad/src/htoad_utils.erl
+++ apps/htoad/src/htoad_utils.erl
@@ -1,5 +1,5 @@
 -module(htoad_utils).
--export([on/2, load/1]).
+-export([on/2, load/1, file/1, render/2, render/3, render/4]).
 
 on([], Plan) ->
     Plan;
@@ -10,3 +10,23 @@ on(What, Plan) ->
 
 load(File) ->
     {load, File}.
+
+file(File) ->
+    filename:absname(filename:join(os:getenv("HTOAD_CWD"), File)).
+
+render(Template, Vars) ->
+    render(Template, [], Vars, []).
+
+render(Template, Vars, RenderOpts) ->
+    render(Template, [], Vars, RenderOpts).
+
+render(Template, Options, Vars, RenderOpts) ->
+    Module = list_to_atom("htoad_template_" ++ integer_to_list(erlang:phash2(Template))),
+    case code:which(Module) of
+        non_existing ->
+            erlydtl:compile(Template, Module, Options);
+        _ ->
+            ok
+    end,
+    {ok, Content} = Module:render(Vars, RenderOpts),
+    Content.
