72c103eda18d8b874da15437bf20f8c52f00a11e
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-02-02 00:39:41 -0800

    htoad_shell should assert shell output for others to be able to process it

--- apps/htoad/src/htoad_shell.erl
+++ apps/htoad/src/htoad_shell.erl
@@ -9,9 +9,9 @@ init(Engine, #init{}, {operating_system_type, unix}) ->
     lager:debug("Initialized htoad_shell"),
     Engine.
 
-command(Engine, #shell{ cmd = Cmd }) ->
+command(Engine, #shell{ cmd = Cmd } = Shell) ->
     lager:debug("Executing shell command: ~s", [Cmd]),
     Result = os:cmd(Cmd),
     lager:debug("Shell output for `~s`: ~s", [Cmd, Result]),
-    Engine.
+    htoad:assert(Engine, {output, Shell, Result}).
 
