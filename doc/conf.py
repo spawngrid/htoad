bdb26498bea12bc97ad97f645f3013537e8bb1ee
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-02-02 16:29:01 -0800

    Minor Sphinx configuration change

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Hypnotoad'
-copyright = u'2012, Spawngrid, Inc.'
+copyright = u'2012, Spawngrid, Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
