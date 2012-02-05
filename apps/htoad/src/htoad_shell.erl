f6882d869133d4f35a59f504136fe9077bf1589a
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-02-04 20:43:28 -0800

    Adds #shell.run_as = superuser option

--- apps/htoad/src/htoad_shell.erl
+++ apps/htoad/src/htoad_shell.erl
@@ -3,13 +3,37 @@
 -include_lib("htoad/include/toadie.hrl").
 -include_lib("htoad/include/stdlib.hrl").
 
--rules([init, command]).
+-rules([init, user, superuser, command_run_in_superuser,
+        command_run_as_superuser, command]).
+
+-neg_rule({command_run_as_superuser, [{?MODULE, superuser}]}).
 
 init(Engine, #init{}, {operating_system_type, unix}) ->
     lager:debug("Initialized htoad_shell"),
-    Engine.
+    htoad:assert(Engine, #shell{ cmd = "whoami" }).
 
-command(Engine, #shell{ cmd = Cmd } = Shell) ->
+user(Engine, {output, #shell{ cmd = "whoami"}, User}) ->
+    lager:debug("Current user: ~s", [User]),
+    htoad:assert(Engine, {user, User}).
+
+superuser(Engine, #init{}, {operating_system_type, unix},
+          {user, "root"}) ->
+    htoad:assert(Engine, {?MODULE, superuser}).
+
+command_run_in_superuser(Engine, #shell{ run_as = superuser } = Shell,
+                         {?MODULE, superuser}) ->
+    command(Engine, Shell).
+
+command_run_as_superuser(Engine, #shell{ cmd = Cmd, run_as = superuser } = Shell,
+                         #file{ path = "/usr/bin/sudo", producer = fs,
+                                content = dontread }) when not {rule, [{?MODULE, superuser}]} ->
+    lager:debug("Executing shell command as a super user (via sudo): ~s", [Cmd]),
+    Result = os:cmd("/usr/bin/sudo -n " ++ Cmd),
+    lager:debug("Shell output for `~s`: ~s", [Cmd, Result]),
+    htoad:assert(Engine, {output, Shell, Result}).
+
+
+command(Engine, #shell{ cmd = Cmd, run_as = undefined } = Shell) ->
     lager:debug("Executing shell command: ~s", [Cmd]),
     Result = os:cmd(Cmd),
     lager:debug("Shell output for `~s`: ~s", [Cmd, Result]),
